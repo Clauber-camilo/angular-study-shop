3303fee15dc0e30ec9bb63b5a1b1a35c4d00e17c
Clauber Camilo <clauber_camilo@live.com>
2018-04-05 21:32:40 -0300

    add a simple product resource

---
+++ src/shared/products.js
@@ -0,0 +1,68 @@
+exports.default = [
+    {
+        name: 'Original Xiaomi Mi Band 2 Smart',
+        original_price: 150.15,
+        final_price: 136.36,
+        discount_pct: 10,
+        description: 'Fitness Tracker: Monitor your activity levels, \
+                      sleep patterns, and more with Mi Band 2 (with US ADI sensors technology), \
+                      which can record your steps, distance, pace, calories burned and active \
+                      minutes, and sync its data via Bluetooth 4.0 to the Mi Fit app, \
+                      so you can check daily, weekly, and monthly history to make a \
+                      scientific workout program for a healthier lifestyle',
+        image: require('../assets/img/mi_band.jpg')
+    },
+    {
+        name: 'Razer DeathAdder Chroma',
+        original_price: 77.85,
+        final_price: null,
+        discount_pct: null,
+        description: 'Perfectly designed to fit snugly under your palm, \
+                    the ergonomic shape of the Razer DeathAdder Chroma gives \
+                    gamers the most comfortable gaming experience ever, \
+                    especially during the most testing of battles.',
+        image: require('../assets/img/razer.jpg')
+    },
+    {
+        name: 'Razer DeathAdder Chroma',
+        original_price: 299.00,
+        final_price: null,
+        discount_pct: null,
+        description: 'Nintendo Switch console, Nintendo Switch dock, \
+                        Joy-Con (L) and Joy-Con (R), and Two Joy-Con strap accessories',
+        image: require('../assets/img/switch.jpg')
+    },
+    {
+        name: 'Original Xiaomi Mi Band 2 Smart',
+        original_price: 150.15,
+        final_price: 136.36,
+        discount_pct: 10,
+        description: 'Fitness Tracker: Monitor your activity levels, \
+                      sleep patterns, and more with Mi Band 2 (with US ADI sensors technology), \
+                      which can record your steps, distance, pace, calories burned and active \
+                      minutes, and sync its data via Bluetooth 4.0 to the Mi Fit app, \
+                      so you can check daily, weekly, and monthly history to make a \
+                      scientific workout program for a healthier lifestyle',
+        image: require('../assets/img/mi_band.jpg')
+    },
+    {
+        name: 'Razer DeathAdder Chroma',
+        original_price: 77.85,
+        final_price: null,
+        discount_pct: null,
+        description: 'Perfectly designed to fit snugly under your palm, \
+                    the ergonomic shape of the Razer DeathAdder Chroma gives \
+                    gamers the most comfortable gaming experience ever, \
+                    especially during the most testing of battles.',
+        image: require('../assets/img/razer.jpg')
+    },
+    {
+        name: 'Razer DeathAdder Chroma',
+        original_price: 299.00,
+        final_price: null,
+        discount_pct: null,
+        description: 'Nintendo Switch console, Nintendo Switch dock, \
+                        Joy-Con (L) and Joy-Con (R), and Two Joy-Con strap accessories',
+        image: require('../assets/img/switch.jpg')
+    }
+]
